31300862e4dc033656356b98d0c0fc4817ea0624
Lucas-Peng-CS <cs20210310@gmail.com>
2021-08-24 22:27:53 +0800

    Confirm user password and unique email & Install express-session connect-flash

--- app.js
+++ app.js
@@ -4,11 +4,22 @@ const db = require('./models')
 const app = express()
 const port = 3000
 const bodyParser = require('body-parser')
+const flash = require('connect-flash')
+const session = require('express-session')
 
 app.engine('handlebars', handlebars({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 
+app.use(session({ secret: 'secret', resave: false, saveUninitialized: false }))
+app.use(flash())
 app.use(bodyParser.urlencoded({ extended: true }))
+
+// 把 req.flash 放到 res.locals 裡面
+app.use((req, res, next) => {
+  res.locals.success_messages = req.flash('success_messages')
+  res.locals.error_messages = req.flash('error_messages')
+  next()
+})
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
