e83967ec3a4ffb58354d899374899277dbb12b82
Lucas-Peng-CS <cs20210310@gmail.com>
2021-08-26 00:23:14 +0800

    Update admin create page for editing usage & Install method-override & Add admin restaurant put feature

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const flash = require('connect-flash')
 const session = require('express-session')
 const passport = require('./config/passport')
 const app = express()
+const methodOverride = require('method-override')
 
 app.engine('handlebars', handlebars({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
@@ -16,6 +17,7 @@ app.use(passport.initialize())
 app.use(passport.session())
 app.use(flash())
 app.use(bodyParser.urlencoded({ extended: true }))
+app.use(methodOverride('_method'))
 
 // 把 req.flash 放到 res.locals 裡面
 app.use((req, res, next) => {
