75b6e150741328aa15ff98b33556744730fb95b6
Lucas-Peng-CS <cs20210310@gmail.com>
2021-08-26 21:24:57 +0800

    Add image to restaurants

--- app.js
+++ app.js
@@ -18,12 +18,13 @@ app.use(passport.session())
 app.use(flash())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
+app.use('/upload', express.static(__dirname + '/upload'))
 
 // 把 req.flash 放到 res.locals 裡面
 app.use((req, res, next) => {
   res.locals.success_messages = req.flash('success_messages')
   res.locals.error_messages = req.flash('error_messages')
-  res.locals.user = req.user 
+  res.locals.user = req.user
   next()
 })
 
