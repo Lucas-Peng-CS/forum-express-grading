10fe8943565750dfae84b34de63f7228a926b206
Lucas-Peng-CS <cs20210310@gmail.com>
2021-08-31 19:55:42 +0800

    Show category name on admin/restaurants & Show category name on admin/restaurant

--- models/category.js
+++ models/category.js
@@ -3,7 +3,7 @@ const {
   Model
 } = require('sequelize')
 module.exports = (sequelize, DataTypes) => {
-  class Restaurant extends Model {
+  class Category extends Model {
     static associate (models) {
       Category.hasMany(models.Restaurant)
     }
